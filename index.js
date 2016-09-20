5e7b091cc5eb7d422d913491b77fa28970c4a67c
meLiu-uuu <1098330898@qq.com>
2016-09-20 18:50:24 +0800

    new

--- index.js
+++ index.js
@@ -24,11 +24,13 @@
 			function signIn(){
 				if (txt.value=="mac") {
 					window.open("desktop.html","_self")
-				}else if (text.value=="yun"){
+				}else if(txt.value=="yun"){
 					window.open("yun.html","_self")
+				}else if(txt.value=="作品"){
+					window.open("i/index.html","_self")
 				}
 				else{
-					alert("口令不对哦，再想想！")
+					alert("输入：作品 二字即可查看作品\n你也可以尝试输入以下关键字登录：\nmac")
 				}
 			}
 			
@@ -40,6 +42,23 @@
 				if (ev.keyCode==13) {
 					signIn();
 				}
+			}
+
+			var listBox=document.getElementById('listbox')
+			document.oncontextmenu=function(ev){
+				var ev=ev||window.event;
+				listBox.style.display='block';
+				listBox.innerHTML='<ul>'+
+					'<li style="text-align:center">欢迎您来到这里！<br/>恭候多时了</li>'+
+					'<li>你可以输入以下关键字进行登录:<p>作品、mac</p></li>'+
+					'<li>欢迎提出指导意见</li>'+
+					'</ul>'
+				listBox.style.top=ev.clientY+'px';
+				listbox.style.left=ev.clientX+'px';
+				return false;
+			}
+			document.onclick=function(){
+				listBox.style.display='none';
 			}
 
 		}
