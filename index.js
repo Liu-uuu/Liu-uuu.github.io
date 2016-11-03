f8214a599e094b78f0df4202f2da907a6113500e
meLiu-uuu <1098330898@qq.com>
2016-11-03 17:56:32 +0800

    首页水平垂直居中优化

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 		window.onload=function () {
 
 			var box=document.getElementById('box')
-			fix(box);
-			window.onresize=function(){
-				fix(box);
-			}
-			function fix(obj){
-				var left=(document.documentElement.clientWidth-obj.offsetWidth)/2;
-				var top=(document.documentElement.clientHeight-obj.offsetHeight)/2;
+			// fix(box);
+			// window.onresize=function(){
+			// 	fix(box);
+			// }
+			// function fix(obj){
+			// 	var left=(document.documentElement.clientWidth-obj.offsetWidth)/2;
+			// 	var top=(document.documentElement.clientHeight-obj.offsetHeight)/2;
 				
 				
-				var scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
+			// 	var scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
 				
-				obj.style.left=left+'px';
-				obj.style.top=(top+scrollTop)*1.1+'px';
-			}
+			// 	obj.style.left=left+'px';
+			// 	obj.style.top=(top+scrollTop)*1.1+'px';
+			// }
 
 
 			var txt=document.getElementById("txt");
