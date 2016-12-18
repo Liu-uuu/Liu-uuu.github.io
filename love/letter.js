0f64c32d808e0013a39f822939d75bd9713b4394
meLiu-uuu <1098330898@qq.com>
2016-12-18 21:42:20 +0800

    letter music

--- love/letter.js
+++ love/letter.js
@@ -1,6 +1,6 @@
 var letters=[
 	{	
-		music:["http://dl.stream.qqmusic.qq.com/C400000xdZuV2LcQ19.m4a?vkey=48C4B42890B58C8A4680F0D33DF98D5ACA2BF38768F25C349FA9C21513D17EE5DADE65DA78B7E81C91045E250BAF752695461FB4D5357F65&guid=3810148505&fromtag=30","http://219.238.2.179/m10.music.126.net/20161217184937/56d90da296fdc76f3339308f6219e75a/ymusic/a120/66a6/09c4/4a0124f064ff25f06e5b4b3ea465295e.mp3?wshc_tag=1&wsts_tag=58551265&wsid_tag=e835892&wsiphost=ipdbm"],
+		music:["http://om5.alicdn.com/158/7158/1136455538/1774490672_1478671781016.mp3?auth_key=e8a9079fae4137cf48916a7fd162c9eb-1482634800-0-null","http://ip.h5.rh03.sycdn.kuwo.cn/98f6d486873ca8e6065923d40a31b8a6/585691b8/resource/a3/17/74/1485368085.aac"],
 		to:"霞宝贝",
 		loveWords:"去年的今天，我在积水潭医院看到你第一眼。匆匆一瞥你就小步跑下楼了。不知道你今天过的怎么样。打电话你总是说很忙的样子。不打电话又怕你觉得我不在意你了。你是很累，满天的工作量，你的身体没有闲下来，但不代表是充实的，因为你的精神是空虚的，你的生活是没有质量的。不要忘了生命中还有很多美好的事情，它会让你更幸福。",
 		from:"龙龙",
